08845ea64ea7ba637d8eca1206ee79bfab6b30e6
Artur Trevizan Benedetti <arturben@marcopolo.com.br>
2025-09-11 12:09:01 -0300

    feat: instalado vue js

--- frontend/src/script.js
+++
@@ -1,7 +0,0 @@
-import { ViewController } from "./controller/ViewController.js";
-
-executaAplicacao();
-
-function executaAplicacao() {
-    ViewController.TelaLogin();
-}
--- frontend/src/service/LoginService.js
+++
@@ -1,9 +0,0 @@
-class LoginService {
-
-    static validaLogin() {
-        return true; // simulação de login bem-sucedido
-    }
-    
-}
-
-export default LoginService;
