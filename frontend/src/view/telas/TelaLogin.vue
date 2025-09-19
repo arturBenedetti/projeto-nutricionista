b7215ca278da2ae24524540c6441f5015c30822e
Artur Benedetti <atbbenedetti@gmail.com>
2025-09-18 21:11:41 -0300

    feat: tela login e componentes de botao e input

---
+++ frontend/src/view/telas/TelaLogin.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <p v-if="false" class="senha-invalida">Usuário ou senha inválidos</p>
+    <div class="forms-login">
+      <div class="input-wrapper">
+        <Input :value="usuario" placeholder="Digite seu usuário" @update:value="usuario = $event" :disabled="false" />
+      </div>
+      <div class="input-wrapper">
+        <Input :value="senha" placeholder="Digite sua senha" @update:value="senha = $event" :disabled="false" />
+      </div>
+      <p>Não possui uma conta? <span>Cadastre-se</span></p>
+    </div>
+    <Button label="Entrar" :color="'teal'" :size="'medium'" :disabled="false" @click="handleSave" />
+  </div>
+</template>
+
+<script>
+import Button from '../../components/buttons/Button.vue';
+import Input from '../../components/inputs/Input.vue';
+
+export default {
+  components: {
+    Button,
+    Input,
+  },
+  methods: {
+    handleSave() {
+      console.log('Botão clicado');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  height: 100vh;
+}
+
+.forms-login {
+  padding-bottom: 5px;
+}
+
+.input-wrapper {
+  margin-bottom: 15px;
+}
+
+.forms-login span {
+    color: #00B5AD;
+}
+
+.senha-invalida {
+    color: red;
+    margin: 2px;
+}
+
+</style>
