ccbe95e33012e770912122d299798d7cc52a8e9d
Artur Benedetti <atbbenedetti@gmail.com>
2025-10-14 20:43:50 -0300

    feat: backend consultar dados paciente

--- backend/src/application/interfaces/IPacienteRepository.ts
+++ backend/src/application/interfaces/IPacienteRepository.ts
@@ -3,4 +3,5 @@ import { Paciente } from "../../domain/entities/Paciente";
 export interface IPacienteRepository {
   save(Paciente: Paciente): Promise<Paciente>;
   findAll(idNutricionista: string): Promise<Paciente[]>;
+  findById(id: string): Promise<Paciente | null>;
 }
