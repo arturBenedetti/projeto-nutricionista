fcde3ef4c5dc372d9f37634c7dadd13b30c3baf8
guiifrank <gsusinfrank@gmail.com>
2025-10-21 19:09:38 -0300

    criando classes BE dieta

---
+++ backend/src/application/controllers/DietaController.ts
@@ -0,0 +1,6 @@
+// import { CreateDietaUseCase } from "/Users/guii/Documents/workspace/projeto-nutricionista/backend/src/domain/useCases/CreateDietaUseCase";
+
+// export class DietaController {
+//     private createDietaUseCase: CreateDietaUseCase;
+
+// }
